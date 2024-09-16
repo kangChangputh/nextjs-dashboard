6b6b5084be0f4459d1d36511d5e10c6d75e1dfea
kangChangputh <126743826+kangChangputh@users.noreply.github.com>
2024-09-16 17:36:41 +0700

    update

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { updateInvoice } from '@/app/lib/action';
+
 
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
 import {
@@ -50,7 +50,7 @@ export default function EditInvoiceForm({
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
-          <div id="customer-error" aria-alive="polite" aria-atomic="true">
+          <div id="customer-error" aria-live="polite" aria-atomic="true">
             {state.errors?.customerId && 
             state.errors.customerId.map((error: string) => (
               <p className='mt-2 text-sm text-red-500' key={error}>
